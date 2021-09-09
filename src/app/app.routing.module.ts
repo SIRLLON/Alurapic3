b7034342a8410eb4501a1a2acb95214416adb4de
Sirllon <sirllonsax@hotmail.com>
2021-09-09 18:16:46 -0300

    Componente de notificacao

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -36,9 +36,13 @@ const routes: Routes = [
         component: PhotoDetailsComponent,
     },
     {
-        path: '**',
-        component: NotFoundComponent
-    }
+      path: 'not-found',
+      component: NotFoundComponent
+  },
+  {
+      path: '**',
+      redirectTo: 'not-found'
+  }
 ];
 
 @NgModule({
--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,7 +1,9 @@
+import { AlertService } from './../../shared/components/alert/alert.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -17,7 +19,9 @@ export class PhotoFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService,
+    private userService: UserService
   ) {}
 
   ngOnInit() {
@@ -32,7 +36,10 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Upload complete', true);
+        this.router.navigate(['/user', this.userService.getUserName()]);
+    });
   }
   handleFile(file: File) {
     this.file = file;
